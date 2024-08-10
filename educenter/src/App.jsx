38f1574551749d23012b598a3fbe8e912b05ef5e
AliNiaz990487 <alik990487@gmail.com>
2024-08-10 22:44:19 +0500

    added preloader to educenter

--- educenter/src/App.jsx
+++ educenter/src/App.jsx
@@ -1,21 +1,23 @@
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Cources from "./Pages/Cources"
-import Events from "./Pages/Events"
-import Blog from "./Pages/Blog"
-import Teacher from "./Pages/Teacher"
-import TeacherSingle from "./Pages/TeacherSingle"
-import Notice from "./Pages/Notice"
-import NoticeDetails from "./Pages/NoticeDetails"
-import Research from "./Pages/Research"
-import Scholarship from "./Pages/Scholarship"
-import CourseDetails from "./Pages/CourseDetails"
-import EventDetails from "./Pages/EventDetails"
-import BlogDetails from "./Pages/BogDetails.jsx"
-import Contact from "./Pages/Contact.jsx"
+import { lazy, Suspense } from "react"
+
+const Home = lazy(() => import("./Pages/Home"))
+const About = lazy(() => import("./Pages/About"))
+const Cources = lazy(() => import("./Pages/Cources"))
+const Events = lazy(() => import("./Pages/Events"))
+const Blog = lazy(() => import("./Pages/Blog"))
+const Teacher = lazy(() => import("./Pages/Teacher"))
+const TeacherSingle = lazy(() => import("./Pages/TeacherSingle"))
+const Notice = lazy(() => import("./Pages/Notice"))
+const NoticeDetails = lazy(() => import("./Pages/NoticeDetails"))
+const Research = lazy(() => import("./Pages/Research"))
+const Scholarship = lazy(() => import("./Pages/Scholarship"))
+const CourseDetails = lazy(() => import("./Pages/CourseDetails"))
+const EventDetails = lazy(() => import("./Pages/EventDetails"))
+const BlogDetails = lazy(() => import("./Pages/BogDetails"))
+const Contact = lazy(() => import("./Pages/Contact"))
 
 import { Routes, Route } from "react-router"
-
+import Loader from "../src/assets/images/preloader.gif"
 const links = {
   "home": "/",
   "about": "/about",
@@ -37,28 +39,34 @@ const links = {
   "contact": "/contact"
 }
 
+const PreLoader = () => (
+  <div className="tw-h-screen tw-bg-center tw-bg-no-repeat" style={{backgroundColor: "#ffdc11", backgroundImage: `url('${Loader}')`}}>
+  </div>
+)
 
 function App() {
 
   return (
-    <Routes>
-      <Route path={links["home"]} element={<Home />} />
-      <Route path={links["about"]} element={<About />} />
-      <Route path={links["cources"]} element={<Cources />} />
-      <Route path={links["events"]} element={<Events />} />
-      <Route path={links["blog"]} element={<Blog />} />
-      <Route path={links["teacher"]} element={<Teacher />} />
-      <Route path={links["teacher-single"]} element={<TeacherSingle />} />
-      <Route path={links["notice"]} element={<Notice />} />
-      <Route path={links["notice-details"]} element={<NoticeDetails />} />
-      <Route path={links["research"]} element={<Research />} />
-      <Route path={links["sholoarship"]} element={<Scholarship />} />
-      <Route path={links["course-detail"]} element={<CourseDetails />} />
-      <Route path={links["event-detail"]} element={<EventDetails />} />
-      <Route path={links["blog-detail"]} element={<BlogDetails />} />
-      <Route path={links["contact"]} element={<Contact />} />
-
-    </Routes>
+    <Suspense fallback={<PreLoader />}>
+      <Routes>
+        <Route path={links["home"]} element={<Home />} />
+        <Route path={links["about"]} element={<About />} />
+        <Route path={links["cources"]} element={<Cources />} />
+        <Route path={links["events"]} element={<Events />} />
+        <Route path={links["blog"]} element={<Blog />} />
+        <Route path={links["teacher"]} element={<Teacher />} />
+        <Route path={links["teacher-single"]} element={<TeacherSingle />} />
+        <Route path={links["notice"]} element={<Notice />} />
+        <Route path={links["notice-details"]} element={<NoticeDetails />} />
+        <Route path={links["research"]} element={<Research />} />
+        <Route path={links["sholoarship"]} element={<Scholarship />} />
+        <Route path={links["course-detail"]} element={<CourseDetails />} />
+        <Route path={links["event-detail"]} element={<EventDetails />} />
+        <Route path={links["blog-detail"]} element={<BlogDetails />} />
+        <Route path={links["contact"]} element={<Contact />} />
+  
+      </Routes>
+    </Suspense>
   )
 }
 export { links }
