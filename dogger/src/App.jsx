2a81250d8b219d966080f657c0d2530a93a798d5
AliNiaz990487 <alik990487@gmail.com>
2024-08-11 16:52:37 +0500

    added preloader to dogger

--- dogger/src/App.jsx
+++ dogger/src/App.jsx
@@ -1,17 +1,25 @@
-import Home from "./pages/Home/Home.jsx";
-import DetailPage from "./pages/DetailPage/DetailPage.jsx";
+import { lazy, Suspense } from "react";
+const Home = lazy(() => import("./pages/Home/Home.jsx"))
+const DetailPage = lazy(() => import("./pages/DetailPage/DetailPage.jsx"))
 import "./assets/global.css";
-
+import { FaPaw } from "react-icons/fa";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const PreLoader = () => (
+  <div className="preloader">
+    <span><FaPaw /></span>
+  </div>
+)
 function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail-page" element={<DetailPage />} />
-      </Routes>
+      <Suspense fallback={<PreLoader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail-page" element={<DetailPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
